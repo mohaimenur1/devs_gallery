eca6cf16343a7659b120a18a2112acdf93d15bf8
Mohai Menur <mmrahmanrahi@gmail.com>
2021-09-15 17:14:42 +0600

    first

---
+++ config/keys_dev.js
@@ -0,0 +1,7 @@
+/** @format */
+
+module.exports = {
+  mongoURI:
+    'mongodb+srv://rahi:SXosKK1Eoy1SeivI@cluster0.poygk.mongodb.net/devconnector?retryWrites=true&w=majority',
+  secretOrKey: 'secret',
+};
